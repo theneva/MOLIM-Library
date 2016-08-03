805e7417312a5e5200062d18b86e6834a9ee41a6
Liam <kingliam28@ntlworld.com>
2016-08-03 19:48:02 +0100

    Create Surface Button component

--- src/components/MoleculeInformation.jsx
+++ src/components/MoleculeInformation.jsx
@@ -1,29 +1,12 @@
 import React from 'react'
 import Molecule from './Molecule.jsx'
+import SurfaceButton from './SurfaceButton.jsx'
 
 const styles = {
   surfaceContainer: {
     display: 'flex',
     flexDirection: 'column'
   },
-  surfaceButtonClickable: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    background: "rgb(242, 242, 242)",
-    height: "5vh",
-    marginBottom: "1vh",
-    border: "solid 1px black"
-  },
-  surfaceButtonNotClickable: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    background: "rgb(242, 242, 242)",
-    height: "5vh",
-    marginBottom: "1vh",
-    border: "solid 1px black"
-  }
 }
 
 export default class MoleculeInformation extends React.Component {
@@ -32,15 +15,9 @@ export default class MoleculeInformation extends React.Component {
       <Molecule constituents={this.props.molecule.molecule}/> {/* lol */}
       <div style={styles.surfaceContainer}>
         {this.props.molecule.potentialEnergySurface.exists ?
-          <a href="#" className="img-rounded col-md-4" style={styles.surfaceButtonClickable}>Potential Energy
-            Surface(s)</a> :
-          <div className="img-rounded col-md-4" style={styles.surfaceButtonNotClickable}>No Potential Energy Surface(s)
-            Available</div>}
+          <SurfaceButton type="Potential Energy Surface(s)"/> : <SurfaceButton type="No Potential Energy Surface(s)"/>}
         {this.props.molecule.dipoleMomentSurface.exists ?
-          <a href="#" className="img-rounded col-md-4" style={styles.surfaceButtonClickable}>Dipole Moment
-            Surface(s)</a> :
-          <div className="img-rounded col-md-4" style={styles.surfaceButtonNotClickable}>No Dipole Moment Surface(s)
-            Available</div>}
+          <SurfaceButton type="Dipole Moment Surface(s)"/> : <SurfaceButton type="No Dipole Moment Surface(s)"/>}
       </div>
     </div>
   }
---
+++ src/components/SurfaceButton.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const styles = {
+  surfaceButtonClickable: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    background: "rgb(242, 242, 242)",
+    height: "5vh",
+    marginBottom: "1vh",
+    border: "solid 1px black"
+  },
+  surfaceButtonNotClickable: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    background: "rgb(242, 242, 242)",
+    height: "5vh",
+    marginBottom: "1vh",
+    border: "solid 1px black"
+  }
+}
+
+export default class SurfaceButton extends React.Component {
+  render() {
+    return this.props.type == "Potential Energy Surface(s)" ?
+      <a href="#" className="img-rounded col-md-4" style={styles.surfaceButtonClickable}>{this.props.type}</a> :
+      <a href="#" className="img-rounded col-md-4" style={styles.surfaceButtonNotClickable}>{this.props.type}</a>
+  }
+}
